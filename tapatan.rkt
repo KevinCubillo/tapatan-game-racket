43e8bd31bcb486985cf9ad152800cf6fbdbb83af
Brandon Retana <branrecha140356@gmail.com>
2023-03-08 22:06:16 -0600

    First 4 places in tapatan

--- tapatan.rkt
+++ tapatan.rkt
@@ -4,59 +4,38 @@
 (require 2htdp/universe)
 
 (define (draw-board board)
-  (overlay (rectangle 720 720 'outline (make-color 200 200 200))
+  (let ((img (rectangle 720 720 'outline (make-color 200 200 200))))
+    (set! img
+          (overlay img
+                   (cond [(equal? (list-ref board 0) "X")                          (place-image (circle 40 "solid" "blue") 120 120 img)]
+                         [(equal? (list-ref board 0) "O")                          (place-image (circle 40 "solid" "red") 120 120 img)]
+                         [else (place-image (text "-" 40 "black") 120 120 img)])
+                   
+                   
+                   (cond [(equal? (list-ref board 1) "X")                          (place-image (circle 40 "solid" "blue") 240 120 img)]
+                         [(equal? (list-ref board 1) "O")                          (place-image (circle 40 "solid" "red") 240 120 img)]
+                         [else (place-image (text "-" 40 "black") 240 120 img)])
+                   
+                   
+                   (cond [(equal? (list-ref board 2) "X")                          (place-image (circle 40 "solid" "blue") 360 120 img)]
+                         [(equal? (list-ref board 2) "O")                          (place-image (circle 40 "solid" "red") 360 120 img)]
+                         [else (place-image (text "-" 40 "black") 360 120 img)])
+                   
+                   
+                   (cond [(equal? (list-ref board 3) "X")                          (place-image (circle 40 "solid" "blue") 120 240 img)]
+                         [(equal? (list-ref board 3) "O")                          (place-image (circle 40 "solid" "red") 120 240 img)]
+                         [else (place-image (text "-" 40 "black") 120 240 img)])))
 
-           (cond [(equal? (list-ref board 0) "X")
-                    (place-image (circle 80 "solid" "blue")
-                                 120 120 (empty-scene 720 720))]
 
-                   [(equal? (list-ref board 0) "O")
-                    (place-image (circle 80 "solid" "red")
-                                 120 120 (empty-scene 720 720))]
+    img))
 
-                   [else (place-image (text "-" 80 "black")
-                                      120 120 (empty-scene 720 720))])
-
-           (cond [(equal? (list-ref board 1) "X")
-                    (place-image (circle 80 "solid" "blue")
-                                 240 120 (empty-scene 720 720))]
-
-                   [(equal? (list-ref board 1) "O")
-                    (place-image (circle 80 "solid" "red")
-                                 240 120 (empty-scene 720 720))]
-
-                   [else (place-image (text "-" 80 "black")
-                                      240 120 (empty-scene 720 720))])
-
-           (cond [(equal? (list-ref board 2) "X")
-                    (place-image (circle 80 "solid" "blue")
-                                 360 120 (empty-scene 720 720))]
-
-                   [(equal? (list-ref board 2) "O")
-                    (place-image (circle 80 "solid" "red")
-                                 360 120 (empty-scene 720 720))]
-
-                   [else (place-image (text "-" 80 "black")
-                                      360 120 (empty-scene 720 720))])
-
-           (cond [(equal? (list-ref board 3) "X")
-                    (place-image (circle 80 "solid" "blue")
-                                 120 240 (empty-scene 720 720))]
-
-                   [(equal? (list-ref board 3) "O")
-                    (place-image (circle 80 "solid" "red")
-                                 120 240 (empty-scene 720 720))]
-
-                   [else (place-image (text "-" 80 "black")
-                                      120 240 (empty-scene 720 720))])))
- 
 
  ; Faltan los otros 6 condicionales para completar el tablero
  ;El tablero solo se dibuja una vez, no se actualiza
  ;El problema es que no se puede modificar el tablero debido a que se envia un empty-scene en cada condicional
 
-(define initial-board '("O" "X" "-"
-                         "-" "O" "-"
+(define initial-board '("O" "X" "X"
+                         "X" "O" "-"
                          "-" "-" "X"))
 
 (big-bang initial-board
